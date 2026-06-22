c064168b41e5a014f4018ccfc336719255c6d74a
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-22 14:55:33 +0200

    feat: add order detail view

--- src/views/OrderDetailView.vue
+++ src/views/OrderDetailView.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const order = ref(null);
+
+onMounted(async () => {
+  const response = await fetch(
+    `http://localhost:3000/orders/${route.params.id}`
+  );
+
+  order.value = await response.json();
+});
+</script>
+
 <template>
-  <h1>Order Details</h1>
+  <div v-if="order">
+    <h1>{{ order.customerName }}</h1>
+
+    <p>Address: {{ order.address }}</p>
+    <p>Flavor: {{ order.flavor }}</p>
+    <p>Topping: {{ order.topping }}</p>
+    <p>Cup or Cone: {{ order.cupOrCone }}</p>
+    <p>Status: {{ order.status }}</p>
+    <p>Total Price: €{{ order.totalPrice }}</p>
+  </div>
 </template>
