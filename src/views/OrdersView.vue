ce5871433f5383cac41360d77f64c8601215c4cb
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-22 13:57:00 +0200

    feat: link orders to order detail page

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -4,18 +4,22 @@ import { ref, onMounted } from "vue";
 const orders = ref([]);
 
 onMounted(async () => {
-    const response = await fetch("http://localhost:3000/orders");
-    orders.value = await response.json();
+  const response = await fetch("http://localhost:3000/orders");
+  orders.value = await response.json();
 });
 </script>
 
 <template>
-    <h1>Orders</h1>
+  <h1>Orders</h1>
 
-    <div v-for="order in orders" :key="order._id">
-        <p>{{ order.customerName }}</p>
-        <p>{{ order.flavor }} - {{ order.topping }}</p>
-        <p>Status: {{ order.status }}</p>
-        <hr />
-    </div>
+  <div v-for="order in orders" :key="order._id">
+    <router-link :to="`/admin/orders/${order._id}`">
+      {{ order.customerName }}
+    </router-link>
+
+    <p>{{ order.flavor }} - {{ order.topping }}</p>
+    <p>Status: {{ order.status }}</p>
+
+    <hr />
+  </div>
 </template>
