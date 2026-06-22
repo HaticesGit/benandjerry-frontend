e9082b684baf52fcc822f5ab7a8d8f4ebd7f7283
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-22 13:04:17 +0200

    feat: connected admin orders to backend

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const orders = ref([]);
+
+onMounted(async () => {
+    const response = await fetch("http://localhost:3000/orders");
+    orders.value = await response.json();
+});
+</script>
+
 <template>
-  <h1>Orders</h1>
+    <h1>Orders</h1>
+
+    <div v-for="order in orders" :key="order._id">
+        <p>{{ order.customerName }}</p>
+        <p>{{ order.flavor }} - {{ order.topping }}</p>
+        <p>Status: {{ order.status }}</p>
+        <hr />
+    </div>
 </template>
