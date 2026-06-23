e571524d887ad284cd8228b5bd2e7eb58617649e
HaticesGit <hatice.baldemir@hotmail.com>
2026-06-23 09:48:07 +0200

    feat: add ice cream configurator

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import AdminLoginView from "../views/AdminLoginView.vue";
 import OrdersView from "../views/OrdersView.vue";
 import OrderDetailView from "../views/OrderDetailView.vue";
+import ConfiguratorView from "../views/ConfiguratorView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-    {
-        path: "/",
-        redirect: "/admin/login",
-    },
     {
         path: "/admin/login",
         component: AdminLoginView,
@@ -23,7 +20,12 @@ const router = createRouter({
         path: "/admin/orders/:id",
         component: OrderDetailView,
     },
+    {
+         path: "/",
+        component: ConfiguratorView,
+    },
     ],
+    
 });
 
 export default router;
---
+++ src/views/ConfiguratorView.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from "vue";
+
+const flavors = [
+    "Chunky Monkey",
+    "Chocolate Fudge Brownie",
+    "Strawberry Doughnut-eee",
+    "Brookieees & Cream",
+];
+
+const toppings = [
+    "Oreo",
+    "Banana",
+    "Sprinkles",
+];
+
+const selectedFlavor = ref("");
+const selectedTopping = ref("");
+const selectedCupOrCone = ref("");
+</script>
+
+<template>
+    <h1>Create Your Ice Cream</h1>
+
+    <h2>Choose a flavor</h2>
+
+    <button v-for="flavor in flavors":key="flavor"@click="selectedFlavor = flavor">
+        {{ flavor }}
+    </button>
+
+  <h2>Choose a topping</h2>
+
+    <button v-for="topping in toppings":key="topping"@click="selectedTopping = topping">
+        {{ topping }}
+    </button>
+
+    <h2>Cup or Cone</h2>
+
+    <button @click="selectedCupOrCone = 'Cup'">
+        Cup
+    </button>
+
+    <button @click="selectedCupOrCone = 'Cone'">
+        Cone
+    </button>
+
+    <h2>Your Selection</h2>
+
+    <p>Flavor:{{ selectedFlavor }}</p>
+    <p>Topping:{{ selectedTopping }}</p>
+    <p>Cup or Cone:{{ selectedCupOrCone }}</p>
+</template>
